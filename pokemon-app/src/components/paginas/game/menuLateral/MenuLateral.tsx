3c8485bfded8622320f2a1968187c23cc3ce5887
Dayne <daynesouza@hotmail.com.br>
2022-04-27 10:20:29 -0300

    27/04

--- pokemon-app/src/components/paginas/game/menuLateral/MenuLateral.tsx
+++ pokemon-app/src/components/paginas/game/menuLateral/MenuLateral.tsx
@@ -1,8 +1,26 @@
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import Icapturado from '../../../../types/capturado'
 
+import './ListaItensMenuLateral.tsx';
+import { ListaItensMenuLateral } from './ListaItensMenuLateral';
+
 export default function (){
+
+    const [menuLateral, setMenuLateral] = useState([
+        {capturado: true, id: uuidv4()},
+        {capturado: true, id: uuidv4()},
+        {capturado: true, id: uuidv4()},
+        {capturado: false, id: uuidv4()},
+        {capturado: false, id: uuidv4()},
+        {capturado: false, id: uuidv4()},
+        {capturado: false, id: uuidv4()},
+    ]);
+
+   
+
+    
     return (
-        <p>menu lateral</p>
+        
     );
 }
